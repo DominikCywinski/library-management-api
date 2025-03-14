9d83dfb7f8ca0ae6f86fdb2e494d7846d929afe5
DominikCywinski <dominik.cywinski988@gmail.com>
2025-03-14 14:57:01 +0100

    Add init database.

--- src/database.py
+++ src/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-
+from src.logger import logging
 
 DATABASE_URL = "postgresql://user:password@db:5432/library"
 
@@ -16,3 +16,8 @@ def get_db():
         yield db
     finally:
         db.close()
+
+
+def init_db():
+    Base.metadata.create_all(bind=engine)
+    logging.info("Database initialized")
